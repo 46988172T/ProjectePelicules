c3134c76fe9b713fc21e80215268e9e323649edb
Leo <46988172t@iespoblenou.org>
2015-11-11 18:13:17 +0100

    Commit 12
    Ya funciona el refresh(). - Prueba GIT

--- app/src/main/java/com/example/leonardomartinez/peliculas/MainActivityFragment.java
+++ app/src/main/java/com/example/leonardomartinez/peliculas/MainActivityFragment.java
@@ -57,7 +57,7 @@ public class MainActivityFragment extends Fragment {
                     adaptador.notifyDataSetChanged();
                     return true;
                 }
-            });*/
+            });**/
 
         return fragment;
     }
